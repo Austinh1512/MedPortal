f7e446b8ff1597ff5ce6e3e077e5b2474aae3a07
austinh1512 <austinhouchens1510@gmail.com>
2026-05-14 00:57:07 -0400

    Config database

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+db = SQLAlchemy(app)
 
 @app.route("/")
 def test():

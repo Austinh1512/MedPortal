a1e36e0b2b558554dfe70bddd339b15266e33b0e
austinh1512 <austinhouchens1510@gmail.com>
2026-06-10 00:07:43 -0400

    Move validation logic to utils

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const validateEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+export const validatePassword = (password) => {
+  // Min 8 characters, at least one uppercase, at least one number, and one special character (!@#$%^&*)
+  return /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(password);
+};
+
